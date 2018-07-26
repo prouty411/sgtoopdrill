20b7918dd553e24e8c1b321651ac2f76487a938c
Sudip Baral <pujasudip@gmail.com>
2018-07-26 10:42:35 -0700

    skeleton of SGT

--- includes/sgt.js
+++ includes/sgt.js
@@ -1,1 +1,48 @@
-sgt.js
+//Student Grade Table
+class SGT{
+    constructor(){
+        this.Classes = [];
+        this.students = [];
+    }
+
+    /**
+     *
+     * @param name
+     */
+    addClass(name){
+
+    }
+
+    /**
+     *
+     * @param name
+     */
+    addStudent(name){
+
+    }
+
+    /**
+     *
+     * @param Class
+     * @param student
+     */
+    addStudentToClass(Class, student){
+
+    }
+
+    /**
+     *
+     * @param Class
+     */
+    listClassRosters(Class){
+
+    }
+
+    /**
+     *
+     * @param student
+     */
+    findStudent(student){
+
+    }
+}
