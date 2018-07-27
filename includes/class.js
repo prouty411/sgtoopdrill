3a49b29d5160c88c6e94713f668518e8cf7b1373
Sean Prouty <prouty_411@hotmail.com>
2018-07-26 18:43:39 -0700

    made addGrade method

--- includes/class.js
+++ includes/class.js
@@ -7,4 +7,10 @@ class Class{
 	addStudent(studentObject){
 		this.roster.push(studentObject);
 	}
+	addGrade(studentObject,this,gradeObject){
+		var grade = new Grade(studentObject,this,gradeObject);
+		this.grades.push(grade);
+		student.addClass(this);
+		student.addGrade(gradeObject);
+	}
 }
