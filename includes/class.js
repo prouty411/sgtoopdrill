130206ab138cedf3767619921d6e69fe3e691500
Sean Prouty <prouty_411@hotmail.com>
2018-07-26 18:27:18 -0700

    added the addStudent method

--- includes/class.js
+++ includes/class.js
@@ -1,7 +1,10 @@
-class Class(){
+class Class{
 	constructor(name){
 		this.roster = [];
 		this.name = name;
 		this.grades= [];
+	}
+	addStudent(studentObject){
+		this.roster.push(student);
 	}
 }
